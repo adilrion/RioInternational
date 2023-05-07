fee633a82e6875ded16ebc3d59da9ad8a532569b
AdilMahmoudRion <adilmahmoudrion@gmail.com>
2023-05-07 21:58:14 +0600

    banner added

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,21 @@
-import { useState } from 'react'
+import { useState } from "react";
 
-import './App.css'
-import { Nav } from './Components/Header/Nav'
+import "./App.css";
+import { Nav } from "./Components/Header/Nav";
+import Banner from "./Components/Banner/Banner";
+import Home from "./Pages/Home/Home";
+import Footer from "./Components/Footer/Footer";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
-   <>
-   
-<Nav></Nav>   
-   </>
-  )
+    <>
+      <Nav></Nav>
+      <Home></Home>
+      <Footer></Footer>
+    </>
+  );
 }
 
-export default App
+export default App;
